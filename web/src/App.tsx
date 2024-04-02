a93bc35f63983ecf05e6cb21bafbbc686c924ade
kimyonghui <kyhui1115@gmail.com>
2024-04-02 21:08:22 +0900

    feat: 라우터 설정

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,17 @@
+import Loading from 'pages/Loading';
+import Login from 'pages/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 const App = () => {
   return (
+    <BrowserRouter>
+      <div className="w-screen h-screen">
+        <Routes>
+          <Route path="/" element={<Loading />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
