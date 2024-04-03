eebd9bd93aff4a0803829e03dbabae0881a063ca
kimyonghui <kyhui1115@gmail.com>
2024-04-03 20:50:28 +0900

    design: 로딩 로고 애니메이션 수정

--- web/src/components/loding/LodingAnimationLogo.tsx
+++ web/src/components/loding/LodingAnimationLogo.tsx
@@ -8,7 +8,7 @@ const LodingAnimationLogo = ({ disappear }: lodingAnimationlogo) => {
   return (
     <div className="flex items-center justify-center bg-orange-100 rounded-full w-44 h-44">
       {disappear ? null : (
-        <div className="relative -left-[4rem] top-24 animate-move-top">
+        <div className="relative -left-[4rem] top-28 animate-move-top">
           <div className="absolute bg-white -left-4 rounded-[45%] rotate-[45deg] w-36 h-36 bottom-10 animate-spin-slow" />
           <div className="absolute bg-white left-0 rounded-[35%] rotate-[45deg] w-40 h-40 bottom-12 animate-spin-slow" />
           <div className="absolute bg-white left-[3rem] rounded-[45%] rotate-[45deg] w-36 h-36 bottom-[3.3rem] animate-spin-slow" />
