698470fcd2e893c34e6fab02bf9549b959412112
kimyonghui <kyhui1115@gmail.com>
2024-04-03 20:37:19 +0900

    feat: react-router 설정

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,30 @@
+import MainLayout from 'layout/MainLayout';
+import Calendar from 'pages/Calendar';
+import Home from 'pages/Home';
+import List from 'pages/List';
 import Loading from 'pages/Loading';
 import Login from 'pages/Login';
+import Profile from 'pages/Profile';
+import TasteSelect from 'pages/TasteSelect';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="w-screen h-screen">
-        <Routes>
-          <Route path="/" element={<Loading />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+      <div className="flex w-screen h-screen md:justify-center">
+        <div className="w-full h-full md:w-1/2 lg:w-1/2 xl:w-1/3 md:border md:border-black">
+          <Routes>
+            <Route path="/" element={<Loading />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/taste-select" element={<TasteSelect />} />
+            <Route element={<MainLayout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
