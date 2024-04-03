e29619570d42a54934fee4f4aed2ea7ffbd4d34c
kimyonghui <kyhui1115@gmail.com>
2024-04-03 20:38:43 +0900

    design: Header UI 구현

---
+++ web/src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import logo2 from '../../assets/logo2.svg';
+
+const Header = () => {
+  return (
+    <div className="flex items-center w-full h-1/10">
+      <img src={logo2} className="ml-4" />
+    </div>
+  );
+};
+
+export default Header;
