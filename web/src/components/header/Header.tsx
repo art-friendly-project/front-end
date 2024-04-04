f1433598f121853f8fd34392add2a30bc7042f79
kimyonghui <kyhui1115@gmail.com>
2024-04-04 22:31:56 +0900

    design: 헤더 높이 수정

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -1,8 +1,8 @@
-import logo2 from '../../assets/logo2.svg';
+import logo2 from '../../assets/art-friendly/logo2.svg';
 
 const Header = () => {
   return (
-    <div className="flex items-center w-full h-1/10">
+    <div className="flex items-center w-full h-2/25">
       <img src={logo2} className="ml-4" />
     </div>
   );
