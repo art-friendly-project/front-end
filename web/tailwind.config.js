ad11baca5c928df9f0b591d30026425350335e74
kimyonghui <kyhui1115@gmail.com>
2024-04-05 22:09:26 +0900

    fix: tailwindCSS 커스텀 오류 수정

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -212,7 +212,7 @@ module.exports = {
       },
       boxShadow: {
         custom:
-          'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;',
+          'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px',
         custom2:
           'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
       },
