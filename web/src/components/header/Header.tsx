0573406bd5d36a44101ec03b422600bfd9d55baa
kimyonghui <kyhui1115@gmail.com>
2024-04-05 17:14:49 +0900

    feat: header 로고 클릭 시 로딩화면으로 이동

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import logo2 from '../../assets/art-friendly/logo2.svg';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
-    <div className="flex items-center w-full h-2/25">
+    <button
+      className="flex items-center w-full h-2/25"
+      onClick={() => {
+        navigate('/');
+      }}
+    >
       <img src={logo2} className="ml-4" />
-    </div>
+    </button>
   );
 };
 
