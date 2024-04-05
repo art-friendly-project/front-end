03150b952ded393073729debb9dedb08388ed9a0
kimyonghui <kyhui1115@gmail.com>
2024-04-05 23:06:54 +0900

    design: 마진 값 패딩으로 변경

--- web/src/components/login/LoginTitle.tsx
+++ web/src/components/login/LoginTitle.tsx
@@ -1,6 +1,6 @@
 const LoginTitle = () => {
   return (
-    <div className="flex flex-col w-full mb-20 ml-10 md:items-center md:ml-0">
+    <div className="flex flex-col w-full pl-10 mb-20 md:items-center md:ml-0">
       <span className="mb-2 text-Display3">Art-Friendly</span>
       <span className="text-Display1-M">예술을 만나고, 나누는 즐거움</span>
     </div>
