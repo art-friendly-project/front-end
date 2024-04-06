ceb8cf11971db8e578e571685aafe06a1b12b795
kimyonghui <kyhui1115@gmail.com>
2024-04-06 21:48:41 +0900

    feat: 인기 전시/행사 목록 IntersectionObserver를 활용한 애니메이션 구현

--- web/src/components/home/popularShow/PopularShow.tsx
+++ web/src/components/home/popularShow/PopularShow.tsx
@@ -1,5 +1,6 @@
 import PosterImg from 'components/common/PosterImg';
 import PosterInfo from './PosterInfo';
+import { useEffect, useRef } from 'react';
 
 interface popularShow {
   id: number;
@@ -9,8 +10,31 @@ interface popularShow {
 }
 
 const PopularShow = ({ name, image, upAndDown, id }: popularShow) => {
+  const showRef = useRef<HTMLButtonElement>(null);
+
+  const observer = new IntersectionObserver((entries) => {
+    const target = entries[0].target as HTMLElement;
+
+    if (entries[0].isIntersecting) {
+      target.style.opacity = '1';
+      target.style.transform = 'translateY(-10px)';
+    } else {
+      target.style.opacity = '0';
+      target.style.transform = 'translateY(10px)';
+    }
+  });
+
+  useEffect(() => {
+    if (showRef.current !== null) {
+      observer.observe(showRef.current);
+    }
+  }, [showRef]);
+
   return (
-    <button className="relative flex items-center px-4 py-2 mb-4 rounded-xl shadow-custom2 w-9/10">
+    <button
+      className="relative flex items-center px-4 py-2 mb-4 duration-1000 translate-y-[10px] opacity-0 rounded-xl shadow-custom2 w-9/10"
+      ref={showRef}
+    >
       <PosterImg width="16" height="16" bgColor="gray-100" image={image} />
       <PosterInfo id={id} name={name} upAndDown={upAndDown} />
     </button>
