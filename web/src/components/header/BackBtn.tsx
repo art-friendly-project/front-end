585662ce2a97fd6c2c91941949eefe2ed3d0fcd5
kimyonghui <kyhui1115@gmail.com>
2024-04-07 22:50:02 +0900

    design: 세부 페이지 헤더 UI 구현

---
+++ web/src/components/header/BackBtn.tsx
@@ -0,0 +1,11 @@
+import { IoIosArrowBack } from 'react-icons/io';
+
+const BackBtn = () => {
+  return (
+    <button className="absolute left-2">
+      <IoIosArrowBack className="w-10 h-10" />
+    </button>
+  );
+};
+
+export default BackBtn;
---
+++ web/src/components/header/DetailHeader.tsx
@@ -0,0 +1,13 @@
+import BackBtn from './BackBtn';
+import HeaderTitle from './HeaderTitle';
+
+const DetailHeader = () => {
+  return (
+    <div className="relative flex items-center justify-center w-full h-2/25">
+      <BackBtn />
+      <HeaderTitle />
+    </div>
+  );
+};
+
+export default DetailHeader;
