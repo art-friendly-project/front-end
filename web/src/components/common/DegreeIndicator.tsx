fed4a592c8288434868627879c16b112b603edb4
kimyonghui <kyhui1115@gmail.com>
2024-04-07 22:48:36 +0900

    refactor: 함수명 변경

--- web/src/components/common/DegreeIndicator.tsx
+++ web/src/components/common/DegreeIndicator.tsx
@@ -1,15 +1,15 @@
-import convertDegreeToColor from 'utils/convertDegreeToColor';
+import changeDegreeToColor from 'utils/changeDegreeToColor';
 
 interface degreeIndicator {
   degree: string;
 }
 
 const DegreeIndicator = ({ degree }: degreeIndicator) => {
-  const degreeColor = convertDegreeToColor(degree);
+  const degreeColor = changeDegreeToColor(degree);
 
   return (
     <span
-      className={`absolute bottom-0 flex items-center justify-center h-6 w-14 rounded-2xl opacity-90 ${degreeColor}`}
+      className={`absolute bottom-0 text-Body1-M flex items-center justify-center h-6 w-14 rounded-2xl opacity-90 ${degreeColor}`}
     >
       {degree}
     </span>
