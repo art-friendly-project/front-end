f4fe73f8cb1ffffce4a6375c352254a574167ed7
kimyonghui <kyhui1115@gmail.com>
2024-04-07 22:49:15 +0900

    rename: FavoriteBtn 공통 컴포넌트로 이동

--- web/src/components/common/FavoriteBtn.tsx
+++ web/src/components/common/FavoriteBtn.tsx
@@ -18,7 +18,7 @@ const FavoriteBtn = ({ favorite }: favoriteBtn) => {
   };
 
   return (
-    <button className="absolute right-7 top-2" onClick={favoriteBtnHandler}>
+    <button className="absolute right-6 top-2" onClick={favoriteBtnHandler}>
       {isFavorite ? (
         <IoHeart className="w-6 h-6 text-orange-100" />
       ) : (
