56cbc913b05574c614c4ed191cbab63a68bbbb8a
kimyonghui <kyhui1115@gmail.com>
2024-04-07 16:04:39 +0900

    design: Header 영역 별도 생성

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -4,14 +4,15 @@ import logo2 from '../../assets/art-friendly/logo2.svg';
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <button
-      className="flex items-center w-full h-2/25"
-      onClick={() => {
-        navigate('/');
-      }}
-    >
-      <img src={logo2} className="ml-4" />
-    </button>
+    <div className="flex items-center w-full h-2/25">
+      <button
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        <img src={logo2} className="ml-4" />
+      </button>
+    </div>
   );
 };
 
