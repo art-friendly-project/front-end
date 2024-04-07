cca3464cc7f83237d35ee029b25162f13840b08d
kimyonghui <kyhui1115@gmail.com>
2024-04-07 15:54:46 +0900

    refactor: endpoint 이동에 따른 네비게이션 이모티콘 활성화 로직 개선

--- web/src/components/navigationBar/NavigationBar.tsx
+++ web/src/components/navigationBar/NavigationBar.tsx
@@ -1,24 +1,54 @@
-import { useState } from 'react';
 import NavigationBtn from './NavigationBtn';
+import homeIconGray from '../../assets/navigation/homeIconGray.svg';
+import listIconGray from '../../assets/navigation/listIconGray.svg';
+import calendarIconGray from '../../assets/navigation/calendarIconGray.svg';
+import profileIconGray from '../../assets/navigation/profileIconGray.svg';
+import homeIconOrange from '../../assets/navigation/homeIconOrange.svg';
+import listIconOrange from '../../assets/navigation/listIconOrange.svg';
+import calendarIconOrange from '../../assets/navigation/calendarIconOrange.svg';
+import profileIconOrange from '../../assets/navigation/profileIconOrange.svg';
 
 const NavigationBar = () => {
-  const [active, setActive] = useState([true, false, false, false]);
-
   return (
     <div className="flex w-full shadow-custom h-1/10 rounded-t-2xl">
-      {navigationNames.map((navigationName, idx) => (
+      {navigations.map((navigation) => (
         <NavigationBtn
-          key={idx}
-          navigationName={navigationName}
-          active={active}
-          setActive={setActive}
-          idx={idx}
+          key={navigation.name}
+          name={navigation.name}
+          endpoint={navigation.endpoint}
+          inActiveIcon={navigation.inActiveIcon}
+          activeIcon={navigation.activeIcon}
         />
       ))}
     </div>
   );
 };
 
-const navigationNames = ['홈', '전시 목록', '캘린더', '프로필'];
+const navigations = [
+  {
+    name: '홈',
+    endpoint: '/home',
+    inActiveIcon: homeIconGray,
+    activeIcon: homeIconOrange,
+  },
+  {
+    name: '전시 목록',
+    endpoint: '/list',
+    inActiveIcon: listIconGray,
+    activeIcon: listIconOrange,
+  },
+  {
+    name: '캘린더',
+    endpoint: '/calendar',
+    inActiveIcon: calendarIconGray,
+    activeIcon: calendarIconOrange,
+  },
+  {
+    name: '프로필',
+    endpoint: '/profile',
+    inActiveIcon: profileIconGray,
+    activeIcon: profileIconOrange,
+  },
+];
 
 export default NavigationBar;
--- web/src/components/navigationBar/NavigationBtn.tsx
+++ web/src/components/navigationBar/NavigationBtn.tsx
@@ -1,71 +1,40 @@
-import homeIconGray from '../../assets/navigation/homeIconGray.svg';
-import listIconGray from '../../assets/navigation/listIconGray.svg';
-import calendarIconGray from '../../assets/navigation/calendarIconGray.svg';
-import profileIconGray from '../../assets/navigation/profileIconGray.svg';
-import homeIconOrange from '../../assets/navigation/homeIconOrange.svg';
-import listIconOrange from '../../assets/navigation/listIconOrange.svg';
-import calendarIconOrange from '../../assets/navigation/calendarIconOrange.svg';
-import profileIconOrange from '../../assets/navigation/profileIconOrange.svg';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 interface navigationBtn {
-  navigationName: string;
-  active: boolean[];
-  setActive: React.Dispatch<React.SetStateAction<boolean[]>>;
-  idx: number;
+  name: string;
+  endpoint: string;
+  inActiveIcon: string;
+  activeIcon: string;
 }
 
 const NavigationBtn = ({
-  navigationName,
-  active,
-  setActive,
-  idx,
+  name,
+  endpoint,
+  inActiveIcon,
+  activeIcon,
 }: navigationBtn) => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.pathname;
 
   const navigationBtnHandler = () => {
-    navigate(navigationEndPoints[idx]);
-
-    setActive((prev) =>
-      prev.map((_, i) => {
-        if (i === idx) return true;
-        else return false;
-      }),
-    );
+    navigate(endpoint);
   };
+
+  const changeInActiveOrActiveIcon = () => {
+    if (!pathname.includes(endpoint)) return inActiveIcon;
+    else return activeIcon;
+  };
+
   return (
     <button
       className="flex flex-col items-center justify-center w-1/4"
-      key={navigationName}
       onClick={navigationBtnHandler}
     >
-      {
-        <img
-          src={
-            active[idx] ? navigationOrangeIcons[idx] : navigationGrayIcons[idx]
-          }
-          className="w-10 h-10 mb-1"
-        />
-      }
-      <span className="text-Body1-M text-gray-80">{navigationName}</span>
+      <img src={changeInActiveOrActiveIcon()} className="w-10 h-10 mb-1" />
+      <span className="text-Body1-M text-gray-80">{name}</span>
     </button>
   );
 };
-
-const navigationEndPoints = ['/home', '/list', '/calendar', '/profile'];
-
-const navigationGrayIcons = [
-  homeIconGray,
-  listIconGray,
-  calendarIconGray,
-  profileIconGray,
-];
-
-const navigationOrangeIcons = [
-  homeIconOrange,
-  listIconOrange,
-  calendarIconOrange,
-  profileIconOrange,
-];
 
 export default NavigationBtn;
