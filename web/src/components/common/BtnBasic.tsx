f9f71b38fbc77e85003d1e05705bbc9a078a3306
kimyonghui <kyhui1115@gmail.com>
2024-04-07 22:48:17 +0900

    feat: BtnBasic 네비게이션 기능 추가

--- web/src/components/common/BtnBasic.tsx
+++ web/src/components/common/BtnBasic.tsx
@@ -1,10 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 interface btnBasic {
   name: string;
+  endpoint: string;
 }
 
-const BtnBasic = ({ name }: btnBasic) => {
+const BtnBasic = ({ name, endpoint }: btnBasic) => {
+  const navigation = useNavigate();
   return (
-    <button className="h-12 bg-orange-100 rounded-lg w-9/10">
+    <button
+      className="h-12 bg-orange-100 rounded-lg w-9/10"
+      onClick={() => {
+        navigation(endpoint);
+      }}
+    >
       <span className="text-white text-Subhead">{name}</span>
     </button>
   );
