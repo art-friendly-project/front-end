53b8afa4e112438c886f8c771e0ee2e8be84d098
kimyonghui <kyhui1115@gmail.com>
2024-04-08 15:53:49 +0900

    design: 어플 최상단 relative 설정 추가

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="flex w-screen h-screen bg-gray-100 md:justify-center">
-        <div className="w-full h-full bg-white md:w-1/2 lg:w-1/2 xl:w-1/3">
+        <div className="relative w-full h-full bg-white md:w-1/2 lg:w-1/2 xl:w-1/3">
           <Routes>
             <Route path="/" element={<Loading />} />
             <Route path="/login" element={<Login />} />
