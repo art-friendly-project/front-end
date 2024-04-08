2558252c4e789d1d94e9ce036aa7070f3b944409
kimyonghui <kyhui1115@gmail.com>
2024-04-08 15:54:00 +0900

    feat: 뒤로가기 기능 구현

--- web/src/components/header/BackBtn.tsx
+++ web/src/components/header/BackBtn.tsx
@@ -1,8 +1,16 @@
 import { IoIosArrowBack } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
 
 const BackBtn = () => {
+  const navigate = useNavigate();
+
   return (
-    <button className="absolute left-2">
+    <button
+      className="absolute left-2"
+      onClick={() => {
+        navigate(-1);
+      }}
+    >
       <IoIosArrowBack className="w-10 h-10" />
     </button>
   );
