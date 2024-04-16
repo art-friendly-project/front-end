abc16df29a321b57c676162150e043362f054cdd
kimyonghui <kyhui1115@gmail.com>
2024-04-16 09:35:21 +0900

    feat: redux reducer 추가

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import locationReducer from './modules/location';
 import toastReducer from './modules/toast';
+import isTestReducer from './modules/isTest';
+import closeNavigateReducer from './modules/closeNavigate';
 
 const store = configureStore({
   reducer: {
     location: locationReducer,
     toast: toastReducer,
+    isTest: isTestReducer,
+    closeNavigate: closeNavigateReducer,
   },
 });
 
