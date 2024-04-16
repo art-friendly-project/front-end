d877192255bb4ee2ce818642b763d72a5f45c26f
kimyonghui <kyhui1115@gmail.com>
2024-04-16 09:34:34 +0900

    design: 토스트 팝업 구현

--- web/src/components/navigationBar/NavigationBar.tsx
+++ web/src/components/navigationBar/NavigationBar.tsx
@@ -25,6 +25,17 @@ const NavigationBar = () => {
           activeIcon={navigation.activeIcon}
         />
       ))}
+      {toast ? (
+        <div
+          className={`absolute -top-14 flex items-center justify-center h-12 left-[5%] w-9/10 rounded-3xl opacity-80 animate-appear-toast ${isFavorite ? 'bg-gray-60' : 'bg-orange-100'}`}
+        >
+          <span className="text-white text-Body3-120">
+            {isFavorite
+              ? '좋아요를 취소했어요.'
+              : '좋아요를 했어요. 캘린더에서 확인해보세요!'}
+          </span>
+        </div>
+      ) : null}
     </div>
   );
 };
