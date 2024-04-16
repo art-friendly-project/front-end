f493242d0224cf6027f25d9964e8fd901394e325
kimyonghui <kyhui1115@gmail.com>
2024-04-16 23:54:00 +0900

    feat: 서비스 준비중 페이지 뒤로가기 기능 추가

--- web/src/components/header/BackBtn.tsx
+++ web/src/components/header/BackBtn.tsx
@@ -1,14 +1,16 @@
 import { IoIosArrowBack } from 'react-icons/io';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const BackBtn = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.pathname;
 
   return (
     <button
       className="absolute left-2"
       onClick={() => {
-        navigate(-1);
+        navigate(pathname === '/prepare-service' ? -2 : -1);
       }}
     >
       <IoIosArrowBack className="w-10 h-10" />
