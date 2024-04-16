6996e40112b24b1a52f0fa80367e5baa2c5c4a4d
kimyonghui <kyhui1115@gmail.com>
2024-04-16 09:17:17 +0900

    refactor: 오른쪽화살표가 있는 버튼 props 수정

--- web/src/components/common/BtnAndRightArrow.tsx
+++ web/src/components/common/BtnAndRightArrow.tsx
@@ -1,21 +1,12 @@
 import { IoIosArrowForward } from 'react-icons/io';
-import { useNavigate } from 'react-router-dom';
 
 interface btnAndRightArrow {
   selectedList?: string[];
-  setIsTest?: React.Dispatch<React.SetStateAction<boolean>>;
-  endpoint: string;
+  fn: () => void;
   name: string;
 }
 
-const BtnAndRightArrow = ({
-  selectedList,
-  endpoint,
-  name,
-  setIsTest,
-}: btnAndRightArrow) => {
-  const navigate = useNavigate();
-
+const BtnAndRightArrow = ({ selectedList, fn, name }: btnAndRightArrow) => {
   const BtnStyle = () => {
     if (selectedList === undefined) {
       return `flex items-center justify-center h-12 text-white rounded-md w-9/10 bg-orange-100`;
@@ -30,10 +21,7 @@ const BtnAndRightArrow = ({
     <button
       className={BtnStyle()}
       disabled={selectedList !== undefined ? !(selectedList.length > 0) : false}
-      onClick={() => {
-        navigate(endpoint);
-        setIsTest !== undefined && setIsTest(true);
-      }}
+      onClick={fn}
     >
       <span className="mr-1 text-Subhead">{name}</span>
       <IoIosArrowForward />
