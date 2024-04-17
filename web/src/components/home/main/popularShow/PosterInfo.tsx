20fab4636472ea8f19a2d987355ff590506121a6
kimyonghui <kyhui1115@gmail.com>
2024-04-17 09:28:34 +0900

    feat: 순위 변동 아이콘 표시 로직 수정

--- web/src/components/home/main/popularShow/PosterInfo.tsx
+++ web/src/components/home/main/popularShow/PosterInfo.tsx
@@ -1,5 +1,6 @@
 import upIcon from '../../../../assets/images/etc/upIcon.svg';
 import downIcon from '../../../../assets/images/etc/downIcon.svg';
+import minusIcon from '../../../../assets/images/etc/minusIcon.svg';
 
 interface posterInfo {
   id: number;
@@ -8,12 +9,17 @@ interface posterInfo {
 }
 
 const PosterInfo = ({ id, name, upAndDown }: posterInfo) => {
+  const rank = (upAndDown: string) => {
+    if (upAndDown[0] === '+') return upIcon;
+    if (upAndDown[0] === '-') return downIcon;
+    else return minusIcon;
+  };
   return (
     <>
       <span className="ml-2 text-Headline">{`0${id}`}</span>
       <span className="ml-2 text-Body3-120">{name}</span>
       <div className="absolute flex right-5 ">
-        <img src={upAndDown[0] === '+' ? upIcon : downIcon} />
+        <img src={rank(upAndDown)} />
         <span className="ml-1 text-Body2-M">{upAndDown[1]}</span>
       </div>
     </>
