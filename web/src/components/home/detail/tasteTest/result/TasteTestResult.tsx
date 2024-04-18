319115a91d4763c422b071907ffd845b508588fd
kimyonghui <kyhui1115@gmail.com>
2024-04-18 10:00:36 +0900

    feat: 취향 분석 자료 수정에 따른 코드 수정

--- web/src/components/home/detail/tasteTest/result/TasteTestResult.tsx
+++ web/src/components/home/detail/tasteTest/result/TasteTestResult.tsx
@@ -3,24 +3,24 @@ import TestResultTitleAndProfile from './TestResultTitleAndProfile';
 import TestFitOrNotFit from './TestFitOrNotFit';
 import BtnBasic from 'components/common/BtnBasic';
 import AnalyzeTestResult from 'utils/AnalyzeTestResult';
-import { tasteTestType } from 'assets/data/tasteTestData';
+import { mbtiType } from 'assets/data/tasteTestData';
 
 const TasteTestResult = () => {
-  const { idx } = AnalyzeTestResult();
-  const result = tasteTestType[idx];
+  const { result } = AnalyzeTestResult();
+  const mbti = mbtiType[result];
 
   const btnhandler = () => {};
 
   return (
     <>
       <TestResultTitleAndProfile
-        title1={result.title1}
-        title2={result.title2}
-        image={result.image}
+        title1={mbti.title1}
+        title2={mbti.title2}
+        image={mbti.image}
       />
-      <TestResultExplain explain={result.explain} />
-      <TestFitOrNotFit result={result} />
-      <div className="flex justify-center mt-32">
+      <TestResultExplain explain={mbti.explain} />
+      <TestFitOrNotFit mbti={mbti} />
+      <div className="flex justify-center mt-16">
         <BtnBasic name="결과 공유하기" fn={btnhandler} disable={false} />
       </div>
     </>
