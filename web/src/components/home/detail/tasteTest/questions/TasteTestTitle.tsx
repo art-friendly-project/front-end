85f4b907ac985f9d81325f5d822aa85284cbadd2
kimyonghui <kyhui1115@gmail.com>
2024-04-18 09:59:42 +0900

    design: css 수정

--- web/src/components/home/detail/tasteTest/questions/TasteTestTitle.tsx
+++ web/src/components/home/detail/tasteTest/questions/TasteTestTitle.tsx
@@ -7,7 +7,7 @@ const TasteTestTitle = ({ currentQuestion, question }: tasteTestTitle) => {
   return (
     <div className="flex flex-col pl-6 mt-10">
       <span className="text-Display2">{`Q${currentQuestion}`}</span>
-      <span className="w-11/12 mt-10 whitespace-break-spaces text-Subhead-M">
+      <span className="w-11/12 mt-10 whitespace-pre-wrap text-Subhead-M">
         {question}
       </span>
     </div>
