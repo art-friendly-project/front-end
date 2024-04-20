3f64c32b07096fb752bbdc5de45d16aa4326dc1c
kimyonghui <kyhui1115@gmail.com>
2024-04-21 00:41:34 +0900

    refactor: 전시 관심 표현 취소 모달 컴포넌트 분리

---
+++ web/src/components/showDetail/rankSection/CancelModalBtnContainer.tsx
@@ -0,0 +1,45 @@
+import { type Dispatch, type SetStateAction } from 'react';
+
+interface cancelModalBtnContainer {
+  setIsModal: Dispatch<SetStateAction<boolean>>;
+  setIsSelectedRanks: Dispatch<SetStateAction<boolean[]>>;
+  cancelIdx: number;
+}
+
+const CancelModalBtnContainer = ({
+  setIsSelectedRanks,
+  setIsModal,
+  cancelIdx,
+}: cancelModalBtnContainer) => {
+  const okBtnHandler = () => {
+    setIsSelectedRanks((prev) =>
+      prev.map((_, i) => {
+        if (i === cancelIdx) return false;
+        else return false;
+      }),
+    );
+
+    setIsModal(false);
+  };
+
+  return (
+    <div className="mt-4">
+      <button
+        className="w-32 h-12 border-2 border-orange-100 rounded-lg text-Subhead text-gray-110"
+        onClick={() => {
+          setIsModal(false);
+        }}
+      >
+        아니요
+      </button>
+      <button
+        className="w-32 h-12 ml-2 text-white bg-orange-100 rounded-lg text-Subhead"
+        onClick={okBtnHandler}
+      >
+        네
+      </button>
+    </div>
+  );
+};
+
+export default CancelModalBtnContainer;
