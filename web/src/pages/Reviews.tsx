1cb70e1243cc82628501d2519fe358d926ec9cad
kimyonghui <kyhui1115@gmail.com>
2024-04-20 22:07:58 +0900

    design: 리뷰 페이지 UI 구현

--- web/src/pages/Reviews.tsx
+++ web/src/pages/Reviews.tsx
@@ -1,10 +1,12 @@
+import BtnContainer from 'components/Reviews/BtnContainer';
 import MemoPad from 'components/Reviews/MemoPad';
 
 const Reviews = () => {
   return (
-    <>
+    <div className="relative flex flex-col items-center w-full h-full pt-10">
       <MemoPad />
-    </>
+      <BtnContainer />
+    </div>
   );
 };
 
