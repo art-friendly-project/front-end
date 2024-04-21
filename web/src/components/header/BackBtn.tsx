880e0fd4b57b41e97b803426bcfd680d8becd5e4
kimyonghui <kyhui1115@gmail.com>
2024-04-21 18:12:09 +0900

    feat: 뒤로가기 시 엔드포인트 리덕스 저장 기능 구현

--- web/src/components/header/BackBtn.tsx
+++ web/src/components/header/BackBtn.tsx
@@ -1,16 +1,28 @@
+import { useAppDispatch } from 'hooks';
 import { IoIosArrowBack } from 'react-icons/io';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { endpointActions } from 'store/modules/endpoint';
 
 const BackBtn = () => {
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const location = useLocation();
   const pathname = location.pathname;
+
+  const moveNavigate = () => {
+    if (pathname === '/prepare-service') {
+      navigate(-2);
+      return;
+    }
+    dispatch(endpointActions.current(pathname));
+    navigate(-1);
+  };
 
   return (
     <button
       className="absolute left-2"
       onClick={() => {
-        navigate(pathname === '/prepare-service' ? -2 : -1);
+        moveNavigate();
       }}
     >
       <IoIosArrowBack className="w-10 h-10" />
