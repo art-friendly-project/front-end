37791f2a45807223305c043c2b70f9fd5aec69a3
kimyonghui <kyhui1115@gmail.com>
2024-04-21 18:17:23 +0900

    feat: 담벼락 확인 후 뒤로가기 시 담벼락 섹션으로 스크롤 이동 기능 구현

--- web/src/pages/ShowDetail.tsx
+++ web/src/pages/ShowDetail.tsx
@@ -4,14 +4,34 @@ import ReviewSection from 'components/showDetail/reviewSection/ReviewSection';
 import RankSection from 'components/showDetail/rankSection/RankSection';
 import ShowInformationSection from 'components/showDetail/InformationSection/ShowInformationSection';
 import PosterSection from 'components/showDetail/posterSection/PosterSection';
+import { useEffect, useRef } from 'react';
+import { useAppSelector } from 'hooks';
+import { selectEndpoint } from 'store/modules/endpoint';
+import useScrollHeight from 'hooks/useScrollHeight';
 
 const ShowDetail = () => {
+  const showDetailRef = useRef<HTMLDivElement>(null);
   const params = useParams();
   const id = Number(params.id);
+  const endpoint = useAppSelector(selectEndpoint);
+  const scrollHeight = useScrollHeight(showDetailRef);
+
   const show = homeShows[id - 1];
 
+  useEffect(() => {
+    if (endpoint.includes('reviews')) {
+      showDetailRef.current?.scrollTo({
+        top: scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  }, [endpoint, scrollHeight]);
+
   return (
-    <div className="relative w-full h-full overflow-y-scroll scrollbar-hide">
+    <div
+      className="relative w-full h-full overflow-y-scroll scrollbar-hide"
+      ref={showDetailRef}
+    >
       <PosterSection show={show} />
       <ShowInformationSection show={show} />
       <RankSection rank={show.rank} />
