a5973b450255f069aff5323961e900e24228f2b1
kimyonghui <kyhui1115@gmail.com>
2024-04-21 18:13:05 +0900

    feat: onClick 함수 분리 및 담벼락 등록 시 내용이 없을 경우 모달창 띄우지 않고 바로 이동

--- web/src/components/header/CloseBtn.tsx
+++ web/src/components/header/CloseBtn.tsx
@@ -1,19 +1,34 @@
-import { IoIosClose } from 'react-icons/io';
-
 import { useState } from 'react';
+import { IoIosClose } from 'react-icons/io';
 import CloseConfirmModal from './CloseConfirmModal';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAppSelector } from 'hooks';
+import { selectIsPostEmpty } from 'store/modules/isPostEmpty';
 
 const CloseBtn = () => {
   const [isModal, setIsModal] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.pathname;
+  const isPostEmpty = useAppSelector(selectIsPostEmpty);
+
+  const btnHandler = () => {
+    if (pathname.includes('review-post')) {
+      if (isPostEmpty) {
+        navigate(-1);
+      } else {
+        setIsModal(true);
+      }
+
+      return;
+    }
+
+    setIsModal(true);
+  };
 
   return (
     <>
-      <button
-        className="absolute right-3"
-        onClick={() => {
-          setIsModal(true);
-        }}
-      >
+      <button className="absolute top-2 right-3" onClick={btnHandler}>
         <IoIosClose className="w-12 h-12" />
       </button>
       {isModal ? <CloseConfirmModal setIsModal={setIsModal} /> : null}
