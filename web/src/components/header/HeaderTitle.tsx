a5a81c756e6578f34ea0d53f5dde61f696ee1729
kimyonghui <kyhui1115@gmail.com>
2024-04-21 18:14:08 +0900

    feat: pathname에 따른 헤더 문구 추가

--- web/src/components/header/HeaderTitle.tsx
+++ web/src/components/header/HeaderTitle.tsx
@@ -9,9 +9,14 @@ const HeaderTitle = () => {
     if (pathname === '/home/taste-test/questions') return '취향분석 테스트';
     if (pathname === '/home/taste-test/result') return '테스트 결과';
     if (pathname.includes('reviews')) return '담벼락';
+    if (pathname.includes('review-post')) return '담벼락 남기기';
   };
 
-  return <span className="text-Subhead">{changeEndpointTotitle()}</span>;
+  return (
+    <span className="absolute top-5 text-Subhead">
+      {changeEndpointTotitle()}
+    </span>
+  );
 };
 
 export default HeaderTitle;
