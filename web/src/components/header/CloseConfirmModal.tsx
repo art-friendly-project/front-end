910c6de3c44494112af70403afab70c2d3914fd9
kimyonghui <kyhui1115@gmail.com>
2024-04-21 18:13:41 +0900

    feat: pathname에 따른 모달 텍스트 변경

--- web/src/components/header/CloseConfirmModal.tsx
+++ web/src/components/header/CloseConfirmModal.tsx
@@ -1,4 +1,4 @@
-import { selectCloseNavigate } from 'store/modules/closeNavigate';
+import { selectEndpoint } from 'store/modules/endpoint';
 import { useAppDispatch, useAppSelector } from 'hooks';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { type Dispatch, type SetStateAction } from 'react';
@@ -12,7 +12,7 @@ const CloseConfirmModal = ({ setIsModal }: closeConfirmModal) => {
   const navigate = useNavigate();
   const location = useLocation();
   const pathname = location.pathname;
-  const closeNavigate = useAppSelector(selectCloseNavigate);
+  const endpoint = useAppSelector(selectEndpoint);
   const dispatch = useAppDispatch();
 
   const clearTest = () => {
@@ -24,14 +24,27 @@ const CloseConfirmModal = ({ setIsModal }: closeConfirmModal) => {
     }
   };
 
+  const modalText = () => {
+    if (
+      pathname === '/home/taste-test/questions' ||
+      pathname === '/home/taste-test/result'
+    ) {
+      return '취향 분석 테스트를 종료할까요?';
+    }
+
+    if (pathname.includes('review-post')) {
+      return '아직 담벼락을 작성 중이에요.\n종료할까요?';
+    }
+  };
+
   return (
     <>
       <div className="absolute top-0 z-10 w-full h-screen bg-black opacity-50" />
       <div className="absolute z-20 flex flex-col items-center justify-center h-48 bg-white w-80 top-80 rounded-xl">
-        <span className="mt-6 text-Headline-M">
-          취향 분석 테스트를 종료할까요?
+        <span className="mt-8 text-center whitespace-pre-wrap text-Headline-M">
+          {modalText()}
         </span>
-        <div className="flex mt-12">
+        <div className="flex mt-4">
           <button
             className="w-32 h-10 mr-2 border-2 border-orange-100 rounded-xl"
             onClick={() => {
@@ -43,7 +56,7 @@ const CloseConfirmModal = ({ setIsModal }: closeConfirmModal) => {
           <button
             className="w-32 h-10 text-white bg-orange-100 rounded-xl"
             onClick={() => {
-              navigate(closeNavigate);
+              navigate(endpoint);
               clearTest();
             }}
           >
