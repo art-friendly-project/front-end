1ecbe7ae31329552fc5b21a169f721d631a31227
kimyonghui <kyhui1115@gmail.com>
2024-04-22 16:19:46 +0900

    feat: 홈 화면 진입 시 endpoint 리덕스 저장

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -3,8 +3,16 @@ import NearbyShowInfoSection from 'components/home/main/nearbyShowSection/Nearby
 import PopularShowSection from 'components/home/main/popularShowSection/PopularShowSection';
 import DeadlineShowSection from 'components/home/main/deadlineShowSection/DeadlineShowSection';
 import IsTestCheck from 'components/home/main/tasteSection/IsTestCheck';
+import { useEffect } from 'react';
+import { useAppDispatch } from 'hooks';
+import { endpointActions } from 'store/modules/endpoint';
 
 const Home = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(endpointActions.current('/home'));
+  }, [endpointActions]);
+
   return (
     <>
       <BannerSection />
