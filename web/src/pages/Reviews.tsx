35e00201822f05dc245d31e76e413fb026eae8c8
kimyonghui <kyhui1115@gmail.com>
2024-04-22 14:49:15 +0900

    design: 담벼락 페이지 UI 구현

--- web/src/pages/Reviews.tsx
+++ web/src/pages/Reviews.tsx
@@ -1,12 +1,50 @@
 import BtnContainer from 'components/Reviews/BtnContainer';
 import MemoPad from 'components/Reviews/MemoPad';
+import StickerModal from 'components/Reviews/StickerModal';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Reviews = () => {
+  const [review, setReview] = useState<review>({
+    id: 0,
+    title: '',
+    content: '',
+    createdAt: '',
+    stickers: [],
+    user: {
+      id: 0,
+      nickName: '',
+      profileImage: '',
+    },
+  });
+
+  const location = useLocation();
+  const reviewData: review = location.state.review;
+  const [isModal, setIsModal] = useState(false);
+  const [isStickerOk, setIsStickerOk] = useState(false);
+
+  useEffect(() => {
+    setReview(reviewData);
+  }, [reviewData]);
+
   return (
-    <div className="relative flex flex-col items-center w-full h-full pt-10">
-      <MemoPad />
-      <BtnContainer />
-    </div>
+    <>
+      <div className="flex flex-col items-center w-full h-full pt-10">
+        <MemoPad review={review} setReview={setReview} />
+        <BtnContainer
+          setIsModal={setIsModal}
+          isStickerOk={isStickerOk}
+          setIsStickerOk={setIsStickerOk}
+        />
+        {isModal ? (
+          <StickerModal
+            setIsModal={setIsModal}
+            setIsStickerOk={setIsStickerOk}
+            setReview={setReview}
+          />
+        ) : null}
+      </div>
+    </>
   );
 };
 
