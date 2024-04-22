64825cb97b5e4fea708d932f17845fe2b089c1e1
kimyonghui <kyhui1115@gmail.com>
2024-04-22 14:46:52 +0900

    chore: 전역 타입 수정 및 추가

---
+++ web/src/type/review.d.ts
@@ -0,0 +1,20 @@
+interface review {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  stickers: sticker[];
+  user: user;
+}
+
+interface sticker {
+  id: number;
+  sticker: string;
+  comments: string;
+}
+
+interface user {
+  id: number;
+  nickName: string;
+  profileImage: string;
+}
