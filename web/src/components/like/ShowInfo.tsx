6ca767f3ef22dbb18703e2fb3fda8fab372bdddd
kimyonghui <kyhui1115@gmail.com>
2024-04-23 16:34:17 +0900

    feat: 관심 목록 전시/행사 기간 지날 시 종료 안내 문구 추가

--- web/src/components/like/ShowInfo.tsx
+++ web/src/components/like/ShowInfo.tsx
@@ -12,10 +12,10 @@ const ShowInfo = ({ name, term }: showInfo) => {
     <div className="flex flex-col items-start mt-2 ml-4">
       <span className="text-Body3 text-gray-110">{name}</span>
       <span className="text-Body2-M text-gray-80">{term}</span>
-      <span className="flex items-center justify-center w-16 h-6 mt-4 text-white bg-orange-100 rounded-2xl text-Body1-M">
-        {remainDay >= 0
-          ? `${remainDay}일 남음`
-          : `${String(remainDay).slice(1)}일 지남`}
+      <span
+        className={`flex items-center justify-center w-16 h-6 mt-4 text-white rounded-2xl text-Body1-M ${remainDay >= 0 ? 'bg-orange-100' : 'bg-purple-90'}`}
+      >
+        {remainDay >= 0 ? `${remainDay}일 남음` : '종료'}
       </span>
     </div>
   );
