766dcfe0bb35003314bfb9f9df031f8eb4c3146a
kimyonghui <kyhui1115@gmail.com>
2024-04-23 13:30:42 +0900

    design: 문구 일부 수정

--- web/src/components/home/detail/nearbyAndLocationShow/ShowEmptyMessage.tsx
+++ web/src/components/home/detail/nearbyAndLocationShow/ShowEmptyMessage.tsx
@@ -1,7 +1,7 @@
 const ShowEmptyMessage = () => {
   return (
     <div className="flex items-center justify-center w-full h-96">
-      <span className="text-Subhead-M">전시/행사 정보가 없어요.</span>
+      <span className="text-Subhead-M">등록된 정보가 없어요.</span>
     </div>
   );
 };
