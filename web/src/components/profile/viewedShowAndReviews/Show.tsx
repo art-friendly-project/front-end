d9c4c86596fec20c97a74d9107f7c54c71b02735
kimyonghui <kyhui1115@gmail.com>
2024-04-24 21:55:48 +0900

    feat: 페이지 이동  시 선택된 메뉴 유지 및 스크롤 위치 수정 기능 구현

--- web/src/components/profile/viewedShowAndReviews/Show.tsx
+++ web/src/components/profile/viewedShowAndReviews/Show.tsx
@@ -1,8 +1,10 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import FavoriteBtn from 'components/common/FavoriteBtn';
 import PosterImg from 'components/common/PosterImg';
 import TemperatureIndicator from 'components/common/TemperatureIndicator';
 import PosterInfo from 'components/home/main/deadlineShowSection/PosterInfo';
+import { useAppDispatch } from 'hooks';
+import { endpointActions } from 'store/modules/endpoint';
 
 const Show = ({
   id,
@@ -14,13 +16,16 @@ const Show = ({
   favorite,
   temperature,
 }: deadlineShow) => {
+  const pathname = useLocation().pathname;
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   return (
     <div className="relative mb-6 w-9/10">
       <button
         className="flex"
         onClick={() => {
+          dispatch(endpointActions.current(pathname));
           navigate(`/shows/${id}`);
         }}
       >
--- web/src/pages/ShowDetail.tsx
+++ web/src/pages/ShowDetail.tsx
@@ -1,23 +1,19 @@
 import { homeShows } from 'mock/mockData';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import ReviewSection from 'components/showDetail/reviewSection/ReviewSection';
 import RankSection from 'components/showDetail/rankSection/RankSection';
 import ShowInformationSection from 'components/showDetail/InformationSection/ShowInformationSection';
 import PosterSection from 'components/showDetail/posterSection/PosterSection';
 import { useEffect, useRef } from 'react';
-import { useAppDispatch, useAppSelector } from 'hooks';
-import { endpointActions, selectEndpoint } from 'store/modules/endpoint';
+import { useAppSelector } from 'hooks';
+import { selectEndpoint } from 'store/modules/endpoint';
 import useScrollHeight from 'hooks/useScrollHeight';
 import useSaveViewedShow from 'hooks/useSaveViewedShow';
 
 const ShowDetail = () => {
   const showDetailRef = useRef<HTMLDivElement>(null);
 
-  const location = useLocation();
-  const pathname = location.pathname;
-
   const endpoint = useAppSelector(selectEndpoint);
-  const dispatch = useAppDispatch();
 
   const params = useParams();
   const id = Number(params.id);
@@ -26,17 +22,13 @@ const ShowDetail = () => {
   const scrollHeight = useScrollHeight(showDetailRef);
 
   useEffect(() => {
-    if (endpoint.includes('reviews') || endpoint === 'myReivew') {
+    if (endpoint.includes('reviews')) {
       showDetailRef.current?.scrollTo({
         top: scrollHeight,
         behavior: 'smooth',
       });
     }
   }, [endpoint, scrollHeight]);
-
-  useEffect(() => {
-    dispatch(endpointActions.current(pathname));
-  }, [pathname]);
 
   useSaveViewedShow(show);
 
