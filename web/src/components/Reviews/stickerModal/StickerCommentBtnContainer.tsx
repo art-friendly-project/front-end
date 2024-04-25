0514c1b5f2381565c268c66601eb4520815afae7
kimyonghui <kyhui1115@gmail.com>
2024-04-25 10:43:50 +0900

    remove: 스티커만 붙이기 버튼 삭제

--- web/src/components/Reviews/stickerModal/StickerCommentBtnContainer.tsx
+++ web/src/components/Reviews/stickerModal/StickerCommentBtnContainer.tsx
@@ -16,22 +16,6 @@ const StickerCommentBtnContainer = ({
   setIsStickerOk,
   setReview,
 }: stickerCommentBtnContainer) => {
-  const onlyStickerBtnHandler = () => {
-    setIsModal(false);
-    setIsStickerOk(true);
-
-    setReview((prev) => {
-      prev.stickers.push({
-        id: prev.stickers.length + 1,
-        sticker,
-        userId: userid,
-        comments: '',
-      });
-
-      return prev;
-    });
-  };
-
   const StickerAndCommentsBtnHandler = () => {
     setIsModal(false);
     setIsStickerOk(true);
@@ -50,14 +34,8 @@ const StickerCommentBtnContainer = ({
   return (
     <div className="flex justify-center w-full mt-4">
       <button
-        className="h-12 border-2 border-orange-100 rounded-lg text-Subhead text-gray-110 w-44"
-        onClick={onlyStickerBtnHandler}
-      >
-        스티커만 붙이기
-      </button>
-      <button
         disabled={text.length === 0}
-        className={`h-12 ml-5 text-white rounded-lg text-Subhead w-44 ${text.length === 0 ? 'bg-orange-50' : 'bg-orange-100'}`}
+        className={`h-12 text-white rounded-lg text-Subhead w-9/10 ${text.length === 0 ? 'bg-orange-50' : 'bg-orange-100'}`}
         onClick={StickerAndCommentsBtnHandler}
       >
         댓글 남기기
