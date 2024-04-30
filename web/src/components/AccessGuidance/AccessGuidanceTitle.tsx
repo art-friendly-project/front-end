d83a3db2043b2dcd412d4624a2ff51893a4f8c55
kimyonghui <kyhui1115@gmail.com>
2024-04-30 21:27:36 +0900

    design: 서비스 접근 권한 이미지 수정

--- web/src/components/AccessGuidance/AccessGuidanceTitle.tsx
+++ web/src/components/AccessGuidance/AccessGuidanceTitle.tsx
@@ -1,9 +1,9 @@
 const AccessGuidanceTitle = () => {
   return (
-    <div className="flex flex-col pl-[5%] mt-10">
-      <span className="text-Headline text-gray-110">앱 접근 권한 안내</span>
-      <span className="mt-2 text-gray-100 text-Body3-M">
-        Art-Friendly 서비스 접근 권한을 확인해 주세요
+    <div className="flex flex-col pl-[5%] mt-20">
+      <span className="text-Display1 text-gray-110">Art-Friendly</span>
+      <span className="mt-4 text-Display1 text-gray-110">
+        서비스 접근 권한을 확인해 주세요.
       </span>
     </div>
   );
--- web/src/components/AccessGuidance/AccessList.tsx
+++ web/src/components/AccessGuidance/AccessList.tsx
@@ -1,36 +1,36 @@
-import { IoLocationOutline } from 'react-icons/io5';
-import { CiFolderOn } from 'react-icons/ci';
-import { IoMdNotificationsOutline } from 'react-icons/io';
 import { LuCalendarCheck } from 'react-icons/lu';
+import location from 'assets/images/etc/location.svg';
+import folder from 'assets/images/etc/folder.svg';
+import notification from 'assets/images/etc/notification.svg';
 
 const AccessList = () => {
   return (
-    <div className="flex flex-col pl-[5%] mt-10">
-      <div className="flex items-center mb-6">
-        <IoLocationOutline className="w-14 h-14" />
+    <div className="flex flex-col pl-[5%] mt-16">
+      <span className="mb-6 text-Headline-M">선택 접근 권한</span>
+      <div className="flex items-center mb-8">
+        <img src={location} className="w-14 h-14" />
         <div className="flex flex-col ml-4">
-          <span className="mb-1 text-Body3-120 text-gray-110">위치 (선택)</span>
-          <span className="text-Body2-M text-gray-110">
-            주변에 있는 전시/행사 검색 시 사용
+          <span className="mb-1 text-Body3-120 text-gray-110">위치</span>
+          <span className="text-Body2-M text-gray-60">
+            주변 전시/행사 검색 시 사용
           </span>
         </div>
       </div>
-      <div className="flex items-center mb-6">
-        <CiFolderOn className="w-14 h-14" />
+      <div className="flex items-center mb-8">
+        <img src={folder} className="w-14 h-14" />
+
         <div className="flex flex-col ml-4">
-          <span className="mb-1 text-Body3-120 text-gray-110">
-            저장 공간 (선택)
-          </span>
-          <span className="text-Body2-M text-gray-110">
+          <span className="mb-1 text-Body3-120 text-gray-110">저장 공간</span>
+          <span className="text-Body2-M text-gray-60">
             프로필 이미지 변경 시 사용
           </span>
         </div>
       </div>
-      <div className="flex items-center mb-6">
-        <IoMdNotificationsOutline className="w-14 h-14" />
+      <div className="flex items-center mb-8">
+        <img src={notification} className="w-14 h-14" />
         <div className="flex flex-col ml-4">
-          <span className="mb-1 text-Body3-120 text-gray-110">알림 (선택)</span>
-          <span className="text-Body2-M text-gray-110">
+          <span className="mb-1 text-Body3-120 text-gray-110">알림</span>
+          <span className="text-Body2-M text-gray-60">
             공지 및 알림 시 사용
           </span>
         </div>
@@ -38,10 +38,8 @@ const AccessList = () => {
       <div className="flex items-center">
         <LuCalendarCheck className="w-14 h-14" />
         <div className="flex flex-col ml-4">
-          <span className="mb-1 text-Body3-120 text-gray-110">달력 (선택)</span>
-          <span className="text-Body2-M text-gray-110">
-            행사 일정 등록 시 사용
-          </span>
+          <span className="mb-1 text-Body3-120 text-gray-110">달력</span>
+          <span className="text-Body2-M text-gray-60">일정 등록 시 사용</span>
         </div>
       </div>
     </div>
