8deb17b54486b06b76e297dd3fb75e7412b184c5
kimyonghui <kyhui1115@gmail.com>
2024-05-03 12:24:08 +0900

    design: 취향분석 추천 페이지 삭제로 인한 UI 변경

--- web/src/components/home/main/tasteSection/TasteTestTitle.tsx
+++ web/src/components/home/main/tasteSection/TasteTestTitle.tsx
@@ -7,7 +7,7 @@ const TasteTestTitle = () => {
         취향 분석 테스트
       </span>
       <span className="w-full mb-10 pl-[5%] text-Body3-M">
-        나에게 맞는 전시를 찾아보아요!
+        나는 어떤 전시 유형일까?
       </span>
       <img src={tasteTestIcon} className="absolute right-10 top-6" />
     </>
