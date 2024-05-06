35551e3c3661e620fee9cf87232fa8b38fadf14c
kimyonghui <kyhui1115@gmail.com>
2024-05-06 14:20:57 +0900

    design: 서비스 이용 약관 페이지 문구 수정

--- web/src/components/termsOfUse/TermsOfUseTitle.tsx
+++ web/src/components/termsOfUse/TermsOfUseTitle.tsx
@@ -3,7 +3,7 @@ const TermsOfUseTitle = () => {
     <div className="flex flex-col pl-[5%] mt-20">
       <span className="text-Display1 text-gray-110">Art-Friendly</span>
       <span className="mt-4 text-Display1 text-gray-110">
-        서비스 이용 약관을 확인해 주세요.
+        서비스 접근 권한을 확인해 주세요.
       </span>
     </div>
   );
