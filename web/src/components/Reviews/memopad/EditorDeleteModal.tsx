7b21314bf7648e31103660486faf5b58877379ad
kimyonghui <kyhui1115@gmail.com>
2024-05-06 14:37:24 +0900

    design: z-index 수정

--- web/src/components/Reviews/memopad/EditorDeleteModal.tsx
+++ web/src/components/Reviews/memopad/EditorDeleteModal.tsx
@@ -10,12 +10,12 @@ const EditorDeleteModal = ({ setIsEditorDeleteModal }: editorDeleteModal) => {
   return (
     <>
       <button
-        className="absolute top-0 z-10 w-full h-screen bg-black opacity-50"
+        className="absolute top-0 z-30 w-full h-screen bg-black opacity-50"
         onClick={() => {
           setIsEditorDeleteModal(false);
         }}
       />
-      <div className="absolute bottom-0 z-20 flex flex-col w-full pt-8 bg-white h-52 rounded-t-xl animate-move-top-regular">
+      <div className="absolute bottom-0 z-40 flex flex-col w-full pt-8 bg-white h-52 rounded-t-xl animate-move-top-regular">
         <EditorDeleteModalTitle />
         <EditorDeleteModalBtnContainer
           setIsEditorDeleteModal={setIsEditorDeleteModal}
