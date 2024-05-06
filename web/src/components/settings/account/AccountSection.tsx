f42f43b1c49a07bf3a382e5ff75ad4db8ac87868
kimyonghui <kyhui1115@gmail.com>
2024-05-06 14:18:56 +0900

    design: 로그아웃, 회원탈퇴 확인 팝업 UI 구현

--- web/src/components/settings/account/AccountSection.tsx
+++ web/src/components/settings/account/AccountSection.tsx
@@ -1,13 +1,18 @@
+import { type Dispatch, type SetStateAction } from 'react';
 import AccountSectionTitle from './AccountSectionTitle';
 import Logout from './Logout';
 import Withdrawal from './Withdrawal';
 
-const AccountSection = () => {
+interface accountSection {
+  setSelectedBtn: Dispatch<SetStateAction<boolean>>;
+}
+
+const AccountSection = ({ setSelectedBtn }: accountSection) => {
   return (
     <div className="flex flex-col pb-10 border-b-10 border-b-gray-00">
       <AccountSectionTitle />
-      <Logout />
-      <Withdrawal />
+      <Logout setSelectedBtn={setSelectedBtn} />
+      <Withdrawal setSelectedBtn={setSelectedBtn} />
     </div>
   );
 };
--- web/src/components/settings/account/Logout.tsx
+++ web/src/components/settings/account/Logout.tsx
@@ -1,8 +1,24 @@
+import { useAppDispatch } from 'hooks';
+import { type Dispatch, type SetStateAction } from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
+import { isModalActions } from 'store/modules/isModal';
 
-const Logout = () => {
+interface logout {
+  setSelectedBtn: Dispatch<SetStateAction<boolean>>;
+}
+
+const Logout = ({ setSelectedBtn }: logout) => {
+  const dispatch = useAppDispatch();
+  const logoutBtnHandler = () => {
+    setSelectedBtn(true);
+    dispatch(isModalActions.setIsModal(true));
+  };
+
   return (
-    <button className="relative flex items-center w-full mt-4 h-12 pl-[5%] active:bg-gray-00">
+    <button
+      className="relative flex items-center w-full mt-4 h-12 pl-[5%] active:bg-gray-00"
+      onClick={logoutBtnHandler}
+    >
       <span className="text-Subhead text-gray-110">로그아웃</span>
       <IoIosArrowForward className="absolute right-[5%] w-7 h-7 text-gray-70" />
     </button>
