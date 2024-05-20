2c5e2141d17cb826208aa9689bfc827acca44fb4
kimyonghui <kyhui1115@gmail.com>
2024-05-20 22:06:25 +0900

    feat: axios 설정 추가

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
+api.interceptors.request.use(
+  (req) => {
+    const accessToken = localStorage.getItem('accessToken');
+    req.headers.authorization = `Bearer ${accessToken}`;
+    return req;
+  },
+  (error) => {
+    throw error;
+  },
+);
+
 export default api;
