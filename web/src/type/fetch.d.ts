ab5dcf14ef36e625c02ec5588b8d82ad965bde1e
kimyonghui <kyhui1115@gmail.com>
2024-05-22 16:49:08 +0900

    chore: type 설정 추가

--- web/src/type/fetch.d.ts
+++ web/src/type/fetch.d.ts
@@ -39,3 +39,74 @@ interface fetchProfile {
     };
   };
 }
+
+interface fetchPopularShow {
+  data: {
+    data: Array<{
+      exhibitionId: number;
+      rank: number;
+      title: string;
+      imageUrl: string;
+      rankShift: string;
+    }>;
+  };
+}
+
+interface fetchEndShow {
+  data: {
+    data: Array<{
+      id: number;
+      title: string;
+      temperature: number;
+      startDate: string;
+      endDate: string;
+      area: string;
+      isLike: false;
+      imageUrl: string;
+    }>;
+  };
+}
+
+interface fetchShow {
+  data: {
+    data: {
+      content: Array<{
+        id: number;
+        title: string;
+        temperature: number;
+        startDate: string;
+        endDate: string;
+        area: string;
+        isLike: false;
+        imageUrl: string;
+      }>;
+      totalPages: number;
+    };
+  };
+}
+
+interface fetchShowDetail {
+  data: {
+    data: {
+      id: number;
+      temperature: number;
+      checkTemperature: null | string;
+      isLike: false;
+      exhibitionInfoRspDto: {
+        id: 3;
+        title: string;
+        startDate: string;
+        endDate: string;
+        place: string;
+        realmName: string;
+        area: string;
+        imageUrl: string;
+        gpsX: 126.99488186959195;
+        gpsY: 37.594828907887845;
+        ticketingUrl: string;
+        phone: string;
+        price: string;
+      };
+    };
+  };
+}
--- web/src/type/show.d.ts
+++ web/src/type/show.d.ts
@@ -1,19 +1,48 @@
-interface favoriteShow {
-  id: number;
-  name: string;
-  term: string;
-  image: string;
-  favorite: boolean;
-  location: string;
+interface popularShow {
+  exhibitionId: number;
+  rank: number;
+  title: string;
+  imageUrl: string;
+  rankShift: string;
 }
 
-interface deadlineShow {
+interface show {
   id: number;
-  showType: string;
-  name: string;
-  location: string;
-  term: string;
-  image: string;
-  favorite: boolean;
-  temperature: string;
+  title: string;
+  area: string;
+  temperature: number;
+  startDate: string;
+  endDate: string;
+  imageUrl: string;
+  isLike: false;
+}
+
+interface favoriteShow {
+  id: number;
+  startDate: string;
+  endDate: string;
+  title: string;
+  imageUrl: string;
+  isLike: boolean;
+  area: string;
+}
+
+interface showDetail {
+  id: number;
+  temperature: number;
+  checkTemperature: null | string;
+  isLike: boolean;
+  exhibitionInfoRspDto: {
+    id: number;
+    title: string;
+    startDate: string;
+    endDate: string;
+    place: string;
+    realmName: string;
+    area: string;
+    imageUrl: string;
+    ticketingUrl: string;
+    phone: string;
+    price: string;
+  };
 }
