ea8060d5ba22c25484fd9ae578b8bd30f8628841
kimyonghui <kyhui1115@gmail.com>
2024-05-26 22:52:36 +0900

    fix: 잘못된 데이터 위치 변경

--- web/src/components/profile/profileInfo/InformationBoard.tsx
+++ web/src/components/profile/profileInfo/InformationBoard.tsx
@@ -13,13 +13,13 @@ const InformationBoard = ({
     <div className="flex justify-center w-full mt-8">
       <div className="flex flex-col items-center justify-center w-1/3 h-10 my-2 border-r-2 border-gray-20">
         <span className="text-Body2-M text-gray-80">작성한 담벼락</span>
-        <span className="mt-1 text-Body3 text-gray-110">{StickerCount}개</span>
-      </div>
-      <div className="flex flex-col items-center justify-center w-1/3 h-10 my-2 border-r-2 border-gray-20">
-        <span className="text-Body2-M text-gray-80">스티커</span>
         <span className="mt-1 text-Body3 text-gray-110">
           {dambyeolagCount}개
         </span>
+      </div>
+      <div className="flex flex-col items-center justify-center w-1/3 h-10 my-2 border-r-2 border-gray-20">
+        <span className="text-Body2-M text-gray-80">스티커</span>
+        <span className="mt-1 text-Body3 text-gray-110">{StickerCount}개</span>
       </div>
       <div className="flex flex-col items-center justify-center w-1/3 h-10 my-2">
         <span className="text-Body2-M text-gray-80">관심 전시</span>
