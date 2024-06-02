fc664ce5782eef73a2840e03322da43191270407
kimyonghui <kyhui1115@gmail.com>
2024-06-03 00:28:18 +0900

    feat: 리뷰 작성 시 재작성 불가하도록 분기 처리

--- web/src/components/showDetail/reviewSection/ReviewPostBtn.tsx
+++ web/src/components/showDetail/reviewSection/ReviewPostBtn.tsx
@@ -6,26 +6,31 @@ import { showIdActions } from 'store/modules/showId';
 
 interface reviewPostBtn {
   id: number;
+  hasDambyeolagWritten: boolean;
 }
 
-const ReviewPostBtn = ({ id }: reviewPostBtn) => {
+const ReviewPostBtn = ({ id, hasDambyeolagWritten }: reviewPostBtn) => {
   const navigate = useNavigate();
   const location = useLocation();
   const pathname = location.pathname;
   const dispatch = useAppDispatch();
 
   return (
-    <button
-      className="absolute flex items-center top-10 text-gray-80 right-[3%] rounded-lg  w-32 h-8 justify-center pl-1 active:bg-gray-00"
-      onClick={() => {
-        dispatch(endpointActions.current(pathname));
-        dispatch(showIdActions.current(id));
-        navigate('/review-post');
-      }}
-    >
-      <span>담벼락 남기기</span>
-      <IoIosArrowForward />
-    </button>
+    <>
+      {hasDambyeolagWritten ? null : (
+        <button
+          className="absolute flex items-center top-10 text-gray-80 right-[3%] rounded-lg  w-32 h-8 justify-center pl-1 active:bg-gray-00"
+          onClick={() => {
+            dispatch(endpointActions.current(pathname));
+            dispatch(showIdActions.current(id));
+            navigate('/review-post');
+          }}
+        >
+          <span>담벼락 남기기</span>
+          <IoIosArrowForward />
+        </button>
+      )}
+    </>
   );
 };
 
