8e33c59fb6c7b6fa8286de43565b2670e80f6fe4
kimyonghui <kyhui1115@gmail.com>
2024-06-03 00:26:07 +0900

    feat: accessToken 재발급 기능 구현

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -1,8 +1,66 @@
-import axios from 'axios';
+import axios, { type AxiosRequestConfig } from 'axios';
 
-const api = axios.create({
-  // baseURL: process.env.REACT_APP_BASE_URL,
-});
+const api = axios.create({});
+
+const refreshToken = localStorage.getItem('refreshToken');
+
+let isRefreshing = false;
+let refreshSubscribers: Array<() => void> = [];
+
+api.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    if (err.config.url === '/oauth/token') throw err;
+    if (err.config.url === '/oauth/token/renew') throw err;
+
+    const originalRequest: AxiosRequestConfig = err.config;
+
+    if (err.response.status === 401) {
+      if (!isRefreshing) {
+        isRefreshing = true;
+
+        return await api
+          .post('/oauth/token/renew', {
+            refreshToken,
+          })
+          .then(async (res) => {
+            localStorage.setItem(
+              'accessToken',
+              String(res.data.data.accessToken),
+            );
+            localStorage.setItem(
+              'refreshToken',
+              String(res.data.data.refreshToken),
+            );
+
+            isRefreshing = false;
+
+            refreshSubscribers.forEach((callback) => {
+              callback();
+            });
+
+            refreshSubscribers = [];
+
+            return await api(originalRequest);
+          })
+          .catch((refreshErr) => {
+            console.error(refreshErr);
+            isRefreshing = false;
+          });
+      } else {
+        return await new Promise((resolve) => {
+          refreshSubscribers.push(() => {
+            resolve(api(originalRequest));
+          });
+        });
+      }
+    }
+
+    throw err;
+  },
+);
 
 api.interceptors.request.use(
   (req) => {
