0118b177505034fa584fada7e5e1e5b1bf5d41e3
kimyonghui <kyhui1115@gmail.com>
2024-06-10 17:18:04 +0900

    feat: 인스타그램 링크 연결

--- web/src/components/footer/Sns.tsx
+++ web/src/components/footer/Sns.tsx
@@ -2,10 +2,15 @@ import { RiInstagramFill } from 'react-icons/ri';
 
 const Sns = () => {
   return (
-    <div className="flex items-center mt-4 mb-2 text-Body1">
+    <a
+      className="flex items-center mt-4 mb-2 text-Body1"
+      href="https://www.instagram.com/art_friendly_?igsh=MTAxOGU0ZzQ5b3duMQ=="
+      target="_blank"
+      rel="noreferrer"
+    >
       <RiInstagramFill />
-      <span>인스타그램</span>
-    </div>
+      <span className="ml-1">인스타그램</span>
+    </a>
   );
 };
 
