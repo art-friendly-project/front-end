2d1ad0d94836de23df1bf39a94da7b6d4928c5e8
kimyonghui <kyhui1115@gmail.com>
2024-06-10 20:04:58 +0900

    design: 카카오 로그인 페이지 UI 수정

--- web/src/pages/KakaoLogin.tsx
+++ web/src/pages/KakaoLogin.tsx
@@ -1,12 +1,12 @@
 import KakaoLoginTitle from 'components/login/KakaoLoginTitle';
-import character from '../assets/images/art-friendly/character.svg';
+import kakaoLoginCharacter from '../assets/images/art-friendly/kakaoLoginCharacter.svg';
 import KakaoLoginBtn from 'components/login/KakaoLoginBtn';
 
 const KakaoLogin = () => {
   return (
     <div className="relative flex flex-col items-center w-full h-screen mt-[22%]">
       <KakaoLoginTitle />
-      <img src={character} className="absolute top-[20%] w-44" />
+      <img src={kakaoLoginCharacter} className="w-72 h-72" />
       <KakaoLoginBtn />
     </div>
   );
