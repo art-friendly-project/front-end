48a3eed5916560e3b294b361578425f33b957626
kimyonghui <kyhui1115@gmail.com>
2024-06-10 16:32:44 +0900

    feat: 전시 온도 소수점 첫째자리 반올림

--- web/src/components/showDetail/posterSection/Thermometer.tsx
+++ web/src/components/showDetail/posterSection/Thermometer.tsx
@@ -5,12 +5,13 @@ interface thermometer {
 }
 
 const Thermometer = ({ temperature }: thermometer) => {
+  console.log(temperature);
   const selectThermometer = changeTemperatureToTermometer(temperature);
   return (
     <div className="absolute z-20 right-4 top-32">
       <img src={selectThermometer} />
       <span className="absolute text-Body1-M top-[5.3rem] text-center right-[0.15rem] w-8 ">
-        {temperature}
+        {temperature.toFixed(1)}
       </span>
     </div>
   );
