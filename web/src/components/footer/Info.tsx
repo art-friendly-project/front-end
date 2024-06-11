db085aa245e9da34945668234d8ed0b9284d318c
kimyonghui <kyhui1115@gmail.com>
2024-06-11 09:27:18 +0900

    chore: 이메일 주소 변경

--- web/src/components/footer/Info.tsx
+++ web/src/components/footer/Info.tsx
@@ -4,7 +4,7 @@ const Info = () => {
       <span>요모조모랩(Yomojomo Lab)</span>
       <span>주소 : 서울 동작구 사당로16아길 62, 2층 201호</span>
       <span>대표전화 : 070-4580-4652</span>
-      <span>이메일 : yomojomolab@gmail.com</span>
+      <span>이메일 : yomojomolab.biz@gmail.com</span>
     </div>
   );
 };
