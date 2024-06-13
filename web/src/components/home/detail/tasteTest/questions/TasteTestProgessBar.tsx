700abde3965134a5c989b2fa9b1f5591ab61b43e
kimyonghui <kyhui1115@gmail.com>
2024-06-13 15:44:05 +0900

    fix: 취향분석 테스트 프로그래스바 UI 오류 해결

--- web/src/components/home/detail/tasteTest/questions/TasteTestProgessBar.tsx
+++ web/src/components/home/detail/tasteTest/questions/TasteTestProgessBar.tsx
@@ -6,13 +6,11 @@ const TasteTestProgessBar = ({ currentQuestion }: tasteTestProgessBar) => {
   const progess = `${(currentQuestion / 12) * 100}%`;
 
   return (
-    <div className="flex justify-center">
-      <div className="h-4 mt-2 w-9/10 bg-gray-00 rounded-2xl">
-        <div
-          className="h-4 duration-500 bg-orange-100 rounded-2xl"
-          style={{ width: progess }}
-        />
-      </div>
+    <div className="absolute h-4 mt-2 top-16 w-9/10 bg-gray-00 rounded-2xl">
+      <div
+        className="h-4 duration-500 bg-orange-100 rounded-2xl"
+        style={{ width: progess }}
+      />
     </div>
   );
 };
