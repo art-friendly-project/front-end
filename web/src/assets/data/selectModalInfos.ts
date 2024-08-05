5a4f0c210004332d16955819e952cb1f10230824
kimyonghui <kyhui1115@gmail.com>
2024-08-05 14:59:02 +0900

    chore: 데이터 변경

--- web/src/assets/data/selectModalInfos.ts
+++ web/src/assets/data/selectModalInfos.ts
@@ -1,6 +1,5 @@
 const selectModalInfos = [
   {
-    type: 'location',
     title1: '내 근처 전시/행사',
     title2: '전시/행사를 보고 싶은 지역을 선택해보세요',
     selects: [
@@ -15,7 +14,6 @@ const selectModalInfos = [
     ],
   },
   {
-    type: 'priority',
     title1: '인기순/최신순',
     title2: '선택한 순으로 전시회를 알려드려요',
     selects: [
