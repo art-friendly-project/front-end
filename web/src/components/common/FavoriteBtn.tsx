9b9539ad2cc66095bdda96b636bafb8a49da6766
kimyonghui <kyhui1115@gmail.com>
2024-08-05 15:00:52 +0900

    feat: react-query 적용

--- web/src/components/common/FavoriteBtn.tsx
+++ web/src/components/common/FavoriteBtn.tsx
@@ -1,52 +1,49 @@
-import { useEffect, useState } from 'react';
 import { IoHeartOutline, IoHeart } from 'react-icons/io5';
 import useToastHandler from 'hooks/useToastHandler';
 import api from 'api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 interface favoriteBtn {
   isLike: boolean;
   id: number;
-  fn?: () => void;
 }
 
-const FavoriteBtn = ({ isLike, id, fn }: favoriteBtn) => {
-  const [isFavorite, setIsFavorite] = useState(false);
+const FavoriteBtn = ({ isLike, id }: favoriteBtn) => {
+  const queryClient = useQueryClient();
+
   const toastHandler = useToastHandler(
-    isFavorite,
+    isLike,
     '관심 목록에 추가했어요',
     '관심 목록에서 취소했어요',
   );
 
-  useEffect(() => {
-    setIsFavorite(isLike);
-  }, [isLike]);
+  const postOrDeleteFavorite = async () => {
+    if (!isLike) {
+      await api.post(`/exhibitions/likes?exhibitionId=${id}`);
+    }
 
-  const favoriteBtnHandler = async () => {
-    try {
-      if (!isFavorite) {
-        await api.post(`/exhibitions/likes?exhibitionId=${id}`);
-      }
-
-      if (isFavorite) {
-        await api.delete(`/exhibitions/likes?exhibitionId=${id}`);
-        if (fn !== undefined) fn();
-      }
-      setIsFavorite((prev) => !prev);
-    } catch (err) {
-      console.error(err);
+    if (isLike) {
+      await api.delete(`/exhibitions/likes?exhibitionId=${id}`);
     }
   };
+
+  const { mutate } = useMutation({
+    mutationFn: postOrDeleteFavorite,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['shows'] });
+    },
+  });
 
   return (
     <>
       <button
         className="absolute right-0 flex items-center justify-center rounded-lg top-1 active:bg-gray-00 w-7 h-7"
         onClick={() => {
-          void favoriteBtnHandler();
+          mutate();
           toastHandler();
         }}
       >
-        {isFavorite ? (
+        {isLike ? (
           <IoHeart className="w-6 h-6 text-orange-100" />
         ) : (
           <IoHeartOutline className="w-6 h-6" />
