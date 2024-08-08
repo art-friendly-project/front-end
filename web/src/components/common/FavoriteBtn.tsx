7baa1bf759ac258f9e93634a284cca50f3b79d25
kimyonghui <kyhui1115@gmail.com>
2024-08-08 16:46:02 +0900

    feat: react-query custom hook 구현

--- web/src/components/common/FavoriteBtn.tsx
+++ web/src/components/common/FavoriteBtn.tsx
@@ -1,7 +1,6 @@
 import { IoHeartOutline, IoHeart } from 'react-icons/io5';
 import useToastHandler from 'hooks/useToastHandler';
-import api from 'api';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { usePostOrDeleteFavorite } from 'hooks/query/usePostOrDeleteFavorite';
 
 interface favoriteBtn {
   isLike: boolean;
@@ -9,30 +8,13 @@ interface favoriteBtn {
 }
 
 const FavoriteBtn = ({ isLike, id }: favoriteBtn) => {
-  const queryClient = useQueryClient();
-
   const toastHandler = useToastHandler(
     isLike,
     '관심 목록에 추가했어요',
     '관심 목록에서 취소했어요',
   );
 
-  const postOrDeleteFavorite = async () => {
-    if (!isLike) {
-      await api.post(`/exhibitions/likes?exhibitionId=${id}`);
-    }
-
-    if (isLike) {
-      await api.delete(`/exhibitions/likes?exhibitionId=${id}`);
-    }
-  };
-
-  const { mutate } = useMutation({
-    mutationFn: postOrDeleteFavorite,
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: ['shows'] });
-    },
-  });
+  const mutate = usePostOrDeleteFavorite(isLike, id);
 
   return (
     <>
