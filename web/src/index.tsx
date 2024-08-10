eb4fe470b7eb491013cef424cafa6912a0089797
kimyonghui <kyhui1115@gmail.com>
2024-08-10 17:17:21 +0900

    feat: redux-persist 적용

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,13 +5,19 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from 'store';
 
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+export const persistor = persistStore(store);
+
 const rootElement = document.getElementById('root');
 
 if (rootElement instanceof HTMLElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>,
   );
 }
--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,4 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage/session';
+
 import locationReducer from './modules/location';
 import toastReducer from './modules/toast';
 import isTestReducer from './modules/isTest';
@@ -11,20 +14,30 @@ import toastTextReducer from './modules/toastText';
 import userIdReducer from './modules/userId';
 import withdrawalReasonReducer from './modules/withdrawalReason';
 
+const reducers = combineReducers({
+  location: locationReducer,
+  toast: toastReducer,
+  isTest: isTestReducer,
+  endpoint: endpointReducer,
+  tasteTest: tasteTestReducer,
+  isModal: isModalReducer,
+  isReviewText: isReviewTextReducer,
+  accessPermissions: accessPermissionsReducer,
+  toastText: toastTextReducer,
+  userId: userIdReducer,
+  withdrawalReason: withdrawalReasonReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['location'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-  reducer: {
-    location: locationReducer,
-    toast: toastReducer,
-    isTest: isTestReducer,
-    endpoint: endpointReducer,
-    tasteTest: tasteTestReducer,
-    isModal: isModalReducer,
-    isReviewText: isReviewTextReducer,
-    accessPermissions: accessPermissionsReducer,
-    toastText: toastTextReducer,
-    userId: userIdReducer,
-    withdrawalReason: withdrawalReasonReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
