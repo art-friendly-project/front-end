c4de5d78830a43e7efe0ebd9a5ee7afd5da21bb7
kimyonghui <kyhui1115@gmail.com>
2024-08-13 13:54:54 +0900

    feat: 이용약관 페이지에서 앱 종료 시 회원 탈퇴 기능 구현

--- web/src/pages/TermsOfUse.tsx
+++ web/src/pages/TermsOfUse.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import BtnContainer from 'components/termsOfUse/BtnContainer';
 import CheckList from 'components/termsOfUse/CheckList';
 import DetailModal from 'components/termsOfUse/DetailModal';
 import TermsOfUseTitle from 'components/termsOfUse/TermsOfUseTitle';
-import { useState } from 'react';
+import useCancelTerms from 'hooks/useCancelTerms';
 
 const TermsOfUse = () => {
+  const navigate = useNavigate();
+
   const [isChecks, setIsChecks] = useState([false, false, false]);
   const [isModal, setIsModal] = useState([false, false]);
+
+  useCancelTerms(navigate);
 
   return (
     <div className="relative flex flex-col items-center w-full h-full">
