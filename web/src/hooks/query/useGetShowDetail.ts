05a618f8361591f88ce446ac8aefa9c8e71c574e
kimyonghui <kyhui1115@gmail.com>
2024-08-15 23:37:05 +0900

    feat: react-query staleTime 설정

--- web/src/hooks/query/useGetShowDetail.ts
+++ web/src/hooks/query/useGetShowDetail.ts
@@ -10,6 +10,7 @@ export const useGetShowDetail = (id: number): showDetail => {
   const { data, error, isError } = useQuery({
     queryKey: ['show', 'detail', id],
     queryFn: async () => await getShowDetail(id),
+    staleTime: 30 * 1000,
   });
 
   if (isError) {
