583e4f3adf875089d5c0bdde0b5fccaf73be4cd0
kimyonghui <kyhui1115@gmail.com>
2024-08-15 16:16:03 +0900

    feat: react-query 전시 상세 리뷰 조회 API 커스텀 훅 구현

---
+++ web/src/hooks/query/useGetReviews.ts
@@ -0,0 +1,32 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import api from 'api';
+import { useState } from 'react';
+
+export const useGetReviews = (id: number) => {
+  const [totalPages, setTotalPages] = useState(0);
+
+  const getReviews = async (page: number, id: number) => {
+    const res: fetchReview = await api.get(
+      `/dambyeolags/lists?exhibitionId=${id}&page=${page}&sortType=popular`,
+    );
+    setTotalPages(res.data.data.totalPages);
+    return res.data.data.content;
+  };
+
+  const { data, fetchNextPage } = useInfiniteQuery({
+    queryKey: ['show', 'reviews', id],
+    queryFn: async ({ pageParam }) => {
+      return await getReviews(pageParam, id);
+    },
+    getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) => {
+      const nextPage = lastPageParam + 1;
+      if (nextPage < totalPages) {
+        return nextPage;
+      }
+    },
+    initialPageParam: 0,
+    staleTime: 30 * 1000,
+  });
+
+  return { data, fetchNextPage };
+};
