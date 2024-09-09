90816574c5a58b99ce3409ae397d0dabbb4bfb63
kimyonghui <kyhui1115@gmail.com>
2024-09-09 11:13:44 +0900

    design: UI 수정

--- web/src/components/home/main/tasteSection/TasteTestTitle.tsx
+++ web/src/components/home/main/tasteSection/TasteTestTitle.tsx
@@ -3,13 +3,13 @@ import tasteTestIcon from 'assets/images/etc/tasteTestIcon.svg';
 const TasteTestTitle = () => {
   return (
     <>
-      <span className="w-full mt-6 mb-2 pl-[5%] text-Headline">
+      <span className="w-full mt-6 mb-1 pl-[5%] text-Headline">
         취향 분석 테스트
       </span>
-      <span className="w-full mb-10 pl-[5%] text-Body3-M text-gray-80">
+      <span className="w-full mb-4 pl-[5%] text-Body3-M text-gray-80">
         나는 어떤 전시 유형일까?
       </span>
-      <img src={tasteTestIcon} className="absolute right-10 top-6" />
+      <img src={tasteTestIcon} className="absolute w-21 h-21 right-4 top-4" />
     </>
   );
 };
